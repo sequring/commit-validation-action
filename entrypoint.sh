752d49ef4ea65d776092dfb3f45acbba98b69d71
Valentin <versus.dev@gmail.com>
2023-02-17 12:57:54 +0100

    move from docker to composite again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e
+
+echo "#################################################"
+echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
 python3 /opt/check_commit.py
+
+echo "#################################################"
+echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
